57ab5e0b5bf73ef56166ce38a4eac2e1ee14b1cb
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-11 19:22:03 +0400

    Update dowhileloop.java

--- dowhileloop.java
+++ dowhileloop.java
@@ -1,16 +1,16 @@
-public class dowhileloop{
-	public static void main(String[] arg){
-		int a=5;
-		do{  
-			System.out.println(a);
-			a++;
-		} 
-		while (a<=10); 
-			int b=5;
-			while (b<=10){  
-  				System.out.println (b); 
-				b++;
-				
-			}
-	}
+public class dowhileloop {
+  public static void main(String[] arg) {
+    int a = 5;
+    do {
+      System.out.println(a);
+      a++;
+    }
+    while (a <= 10);
+    int b = 5;
+    while (b <= 10) {
+      System.out.println(b);
+      b++;
+
+    }
+  }
 }
